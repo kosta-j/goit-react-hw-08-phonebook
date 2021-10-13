658933e72b40050f7450f48813179474cfd8a586
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2021-10-13 16:04:29 +0300

    sign up component has re-written with MUI

--- src/views/SignupView/SignupView.jsx
+++ src/views/SignupView/SignupView.jsx
@@ -1,8 +1,10 @@
-import { Field, Form, Formik } from 'formik';
+import { useFormik } from 'formik';
 import { Link } from 'react-router-dom';
-import * as Yup from 'yup';
 import { Section } from '../../components';
+import * as Yup from 'yup';
 import { routes } from '../../utils/routes';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 const userNameValidation =
   /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/;
@@ -23,128 +25,70 @@ const SignupSchema = Yup.object().shape({
 });
 
 export default function SignupView() {
-  // const [name, setName] = useState('');
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
-  // const dispatch = useDispatch();
-
-  // const handleChange = e => {
-  //   const { name, value } = e.currentTarget;
-  //   switch (name) {
-  //     case 'name':
-  //       setName(value);
-  //       break;
-  //     case 'email':
-  //       setEmail(value);
-  //       break;
-  //     case 'password':
-  //       setPassword(value);
-  //       break;
-  //     default:
-  //       console.log('this data is not supported');
-  //   }
-  // };
-
-  const handleSubmit = e => {
-    // e.preventDefault();
-    // dispatch(authOperations.register({ name, email, password }));
-    console.log('form submitted');
-  };
+  const formik = useFormik({
+    initialValues: {
+      userName: '',
+      email: '',
+      password: '',
+    },
+    validationSchema: SignupSchema,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 3));
+    },
+  });
 
   return (
-    <Section title="Sign up page">
-      <Formik
-        initialValues={{
-          userName: '',
-          email: '',
-          password: '',
-        }}
-        validationSchema={SignupSchema}
-        onSubmit={values => {
-          // same shape as initial values
-          console.log(values);
-        }}
-      >
-        {({ errors, touched, isSubmitting }) => (
-          <Form>
-            <label htmlFor="userName">Name: </label>
-            <Field name="userName" placeholder="Enter name" />
-            {errors.userName && touched.userName ? (
-              <div>{errors.userName}</div>
-            ) : null}
-
-            <label htmlFor="email">Email: </label>
-            <Field name="email" type="email" placeholder="Enter email" />
-            {errors.email && touched.email ? <div>{errors.email}</div> : null}
-
-            <label htmlFor="password">Password: </label>
-            <Field
-              name="password"
-              type="password"
-              placeholder="Enter password"
-            />
-            {errors.password && touched.password ? (
-              <div>{errors.password}</div>
-            ) : null}
-            {console.log(Object.keys(errors).length)}
-            <button
-              type="submit"
-              disabled={
-                isSubmitting ||
-                Object.keys(errors).length !== 0 ||
-                Object.keys(touched).length === 0
-              }
-            >
-              Sign up
-            </button>
-          </Form>
-        )}
-      </Formik>
-      <Link to={routes.login}>Have an account? Login</Link>
-      {/* <form onSubmit={handleSubmit}>
-        <label>
-          <p className={s.inputTitle}>Name</p>
-          <input
-            type="text"
-            name="name"
-            className={s.input}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-            required
-            value={name}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          <p className={s.inputTitle}>email</p>
-          <input
-            type="email"
-            name="email"
-            className={s.input}
-            pattern="(\w\.?)+@[\w\.-]+\.\w{2,}"
-            title="email может быть формата something@else.tld"
-            required
-            value={email}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          <p className={s.inputTitle}>password</p>
-          <input
-            type="password"
-            name="password"
-            className={s.input}
-            //   pattern="(\w\.?)+@[\w\.-]+\.\w{2,}"
-            title="just password"
-            required
-            value={password}
-            onChange={handleChange}
-          />
-        </label>
-        <button type="submit" className={s.submitBtn}>
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          fullWidth
+          id="userName"
+          name="userName"
+          label="Name"
+          value={formik.values.userName}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.userName && Boolean(formik.errors.userName)}
+          helperText={formik.touched.userName && formik.errors.userName}
+        />
+        <TextField
+          fullWidth
+          id="email"
+          name="email"
+          label="Email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+        <TextField
+          fullWidth
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+        />
+        <Button
+          color="primary"
+          variant="contained"
+          fullWidth
+          type="submit"
+          disabled={
+            formik.isSubmitting ||
+            Object.keys(formik.errors).length !== 0 ||
+            Object.keys(formik.touched).length === 0
+          }
+        >
           Sign up
-        </button>
-      </form> */}
-    </Section>
+        </Button>
+      </form>
+      <Link to={routes.login}>Have an account? - Log in</Link>
+    </div>
   );
 }
