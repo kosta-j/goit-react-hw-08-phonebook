568c7f4b2d92bbad4c66f490fd85aa37ad48f518
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2021-10-16 20:18:57 +0300

    persist turned on

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,15 +1,15 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-// import {
-//   FLUSH,
-//   PAUSE,
-//   PERSIST,
-//   persistReducer,
-//   persistStore,
-//   PURGE,
-//   REGISTER,
-//   REHYDRATE,
-// } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { authReducer } from './auth';
 
 import contactsReducer from './reducers';
@@ -17,29 +17,32 @@ import contactsReducer from './reducers';
 console.log('authReducer:', authReducer);
 console.log('contactsReducer:', contactsReducer);
 
-// const authPersistConfig = {
-//   key: 'auth',
-//   storage,
-//   whitelist: ['token'],
-// };
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
-// const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
+const persistedAuthReducer = persistReducer(
+  authPersistConfig,
+  authReducer.default,
+);
 
-// const middleware = [
-//   ...getDefaultMiddleware({
-//     serializableCheck: {
-//       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//     },
-//   }),
-// ];
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer.default,
+    auth: persistedAuthReducer,
     contacts: contactsReducer,
   },
-  // middleware: middleware,
+  middleware: middleware,
   devTools: process.env.NODE_ENV === 'development',
 });
 
-// export const persistor = persistStore(store);
+export const persistor = persistStore(store);
