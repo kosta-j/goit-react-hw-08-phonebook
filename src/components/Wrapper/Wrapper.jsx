bb443eb047b1d7d046101469dbc1938334732b28
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2021-10-18 16:20:09 +0300

    add lazy loading

--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -1,14 +1,17 @@
-import { useEffect } from 'react';
+import { useEffect, Suspense, lazy } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Switch } from 'react-router';
-import { AuthView } from '../../views';
-import { ContactsView } from '../../views';
-import { HomeView } from '../../views';
-import { SignupView } from '../../views';
 import { AppBar } from '../';
 import { routes } from '../../utils/routes';
 import s from './Wrapper.module.css';
 import { authOperations } from '../../Redux/auth';
+
+const HomeView = lazy(() => import('../../views/HomeView/HomeView'));
+const ContactsView = lazy(() =>
+  import('../../views/ContactsView/ContactsView'),
+);
+const AuthView = lazy(() => import('../../views/AuthView/AuthView'));
+const SignupView = lazy(() => import('../../views/SignupView/SignupView'));
 
 export default function Wrapper() {
   const dispatch = useDispatch();
@@ -19,10 +22,12 @@ export default function Wrapper() {
     <div className={s.wrapper}>
       <AppBar />
       <Switch>
-        <Route path={routes.home} component={HomeView} exact />
-        <Route path={routes.register} component={SignupView} />
-        <Route path={routes.login} component={AuthView} />
-        <Route path={routes.contacts} component={ContactsView} />
+        <Suspense fallback={<p>Loading...</p>}>
+          <Route exact path={routes.home} component={HomeView} />
+          <Route path={routes.register} component={SignupView} />
+          <Route path={routes.login} component={AuthView} />
+          <Route path={routes.contacts} component={ContactsView} />
+        </Suspense>
       </Switch>
     </div>
   );
